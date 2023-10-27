ad2b0060190a27b5942cf59c166cb1031fad8eb8
lachlanm-git <84654206+lachlanm-git@users.noreply.github.com>
2023-10-27 12:37:52 +0800

    apply zig fmt

--- build.zig
+++ build.zig
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     exe.addIncludePath(.{ .path = "examples/AllTests" });
 
     // CPPUTEST_USE_GCOV, -lgcov
-    exe.addCSourceFiles( &.{
+    exe.addCSourceFiles(&.{
         // the "application" to be put under test
         "examples/ApplicationLib/CircularBuffer.cpp",
         "examples/ApplicationLib/EventDispatcher.cpp",
@@ -56,7 +56,6 @@ pub fn build(b: *std.Build) void {
 
     if (debug_print_build_info) print_build_info(exe);
 
-
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
@@ -80,61 +79,57 @@ fn build_cpputest(b: *std.Build, libtarget: std.zig.CrossTarget) *std.Build.Comp
     lib.linkLibCpp();
     lib.strip = true;
 
-    lib.addIncludePath(.{ .path = "include"});
+    lib.addIncludePath(.{ .path = "include" });
 
     if (debug_print_build_info) print_build_info(lib);
 
     const t = lib.target_info.target;
 
-    lib.addCSourceFiles(
-        &.{
-            // CppUTest
-            "src/CppUTest/CommandLineArguments.cpp",
-            "src/CppUTest/CommandLineTestRunner.cpp",
-            "src/CppUTest/JUnitTestOutput.cpp",
-            "src/CppUTest/TeamCityTestOutput.cpp",
-            "src/CppUTest/MemoryLeakDetector.cpp",
-            "src/CppUTest/MemoryLeakWarningPlugin.cpp",
-            "src/CppUTest/SimpleMutex.cpp",
-            "src/CppUTest/SimpleString.cpp",
-            "src/CppUTest/SimpleStringInternalCache.cpp",
-            "src/CppUTest/TestFailure.cpp",
-            "src/CppUTest/TestFilter.cpp",
-            "src/CppUTest/TestHarness_c.cpp",
-            "src/CppUTest/TestMemoryAllocator.cpp",
-            "src/CppUTest/TestOutput.cpp",
-            "src/CppUTest/TestPlugin.cpp",
-            "src/CppUTest/TestRegistry.cpp",
-            "src/CppUTest/TestResult.cpp",
-            "src/CppUTest/TestTestingFixture.cpp",
-            "src/CppUTest/Utest.cpp",
+    lib.addCSourceFiles(&.{
+        // CppUTest
+        "src/CppUTest/CommandLineArguments.cpp",
+        "src/CppUTest/CommandLineTestRunner.cpp",
+        "src/CppUTest/JUnitTestOutput.cpp",
+        "src/CppUTest/TeamCityTestOutput.cpp",
+        "src/CppUTest/MemoryLeakDetector.cpp",
+        "src/CppUTest/MemoryLeakWarningPlugin.cpp",
+        "src/CppUTest/SimpleMutex.cpp",
+        "src/CppUTest/SimpleString.cpp",
+        "src/CppUTest/SimpleStringInternalCache.cpp",
+        "src/CppUTest/TestFailure.cpp",
+        "src/CppUTest/TestFilter.cpp",
+        "src/CppUTest/TestHarness_c.cpp",
+        "src/CppUTest/TestMemoryAllocator.cpp",
+        "src/CppUTest/TestOutput.cpp",
+        "src/CppUTest/TestPlugin.cpp",
+        "src/CppUTest/TestRegistry.cpp",
+        "src/CppUTest/TestResult.cpp",
+        "src/CppUTest/TestTestingFixture.cpp",
+        "src/CppUTest/Utest.cpp",
 
-            // CppUTestExt
-            "src/CppUTestExt/CodeMemoryReportFormatter.cpp",
-            "src/CppUTestExt/GTest.cpp",
-            "src/CppUTestExt/IEEE754ExceptionsPlugin.cpp",
-            "src/CppUTestExt/MemoryReportAllocator.cpp",
-            "src/CppUTestExt/MemoryReporterPlugin.cpp",
-            "src/CppUTestExt/MemoryReportFormatter.cpp",
-            "src/CppUTestExt/MockActualCall.cpp",
-            "src/CppUTestExt/MockExpectedCall.cpp",
-            "src/CppUTestExt/MockExpectedCallsList.cpp",
-            "src/CppUTestExt/MockFailure.cpp",
-            "src/CppUTestExt/MockNamedValue.cpp",
-            "src/CppUTestExt/MockSupport.cpp",
-            "src/CppUTestExt/MockSupportPlugin.cpp",
-            "src/CppUTestExt/MockSupport_c.cpp",
-            "src/CppUTestExt/OrderedTest.cpp",
-        }, &.{
-            "-Werror"
-        });
+        // CppUTestExt
+        "src/CppUTestExt/CodeMemoryReportFormatter.cpp",
+        "src/CppUTestExt/GTest.cpp",
+        "src/CppUTestExt/IEEE754ExceptionsPlugin.cpp",
+        "src/CppUTestExt/MemoryReportAllocator.cpp",
+        "src/CppUTestExt/MemoryReporterPlugin.cpp",
+        "src/CppUTestExt/MemoryReportFormatter.cpp",
+        "src/CppUTestExt/MockActualCall.cpp",
+        "src/CppUTestExt/MockExpectedCall.cpp",
+        "src/CppUTestExt/MockExpectedCallsList.cpp",
+        "src/CppUTestExt/MockFailure.cpp",
+        "src/CppUTestExt/MockNamedValue.cpp",
+        "src/CppUTestExt/MockSupport.cpp",
+        "src/CppUTestExt/MockSupportPlugin.cpp",
+        "src/CppUTestExt/MockSupport_c.cpp",
+        "src/CppUTestExt/OrderedTest.cpp",
+    }, &.{"-Werror"});
 
-    lib.addCSourceFiles(
-        switch(t.os.tag) {
-            .windows => &.{ "src/Platforms/Dos/UtestPlatform.cpp" },
-            .linux, .macos => &.{ "src/Platforms/Gcc/UtestPlatform.cpp" },
-            else => unreachable, // @panic("can only support Windows or Linux")
-        }, &.{});
+    lib.addCSourceFiles(switch (t.os.tag) {
+        .windows => &.{"src/Platforms/Dos/UtestPlatform.cpp"},
+        .linux, .macos => &.{"src/Platforms/Gcc/UtestPlatform.cpp"},
+        else => unreachable, // @panic("can only support Windows or Linux")
+    }, &.{});
 
     return lib;
 }
@@ -156,5 +151,4 @@ fn print_build_info(lib: *std.Build.CompileStep) void {
     // -Doptimize=ReleaseFast
     // -Doptimize=ReleaseSafe
     std.debug.print("  release-mode: {}\n", .{lib.optimize});
-
 }
